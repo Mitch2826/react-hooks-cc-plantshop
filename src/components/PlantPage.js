b2e3fd1008b00291777be1471bb7cf1fde441381
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-08-26 00:38:32 +0300

    plant card

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,47 @@
-import React from "react";
+import {useState, useEffect} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+
+  const [plants, setPlants] = useState ([]);
+
+  const [searchTerm, setSearchTerm] = useState("");
+
+useEffect(() => {
+ fetch ("http://localhost:6001/plants")
+  .then((response) => response.json())
+  .then((plantsData => setPlants(plantsData)));
+},
+  []);
+
+function handleAddPlant(newPlant) {
+  setPlants([...plants, newPlant]);
+}
+
+
+function handleToggleSoldOut(id) {
+  setPlants(plants.map(plant => {
+    if (plant.id === id){ 
+      return {...plant, soldOut: !plant.soldOut}
+} 
+    return plant;
+}));
+}
+
+function handleSearchChange(term) {
+  setSearchTerm(term);
+}
+
+const filteredPlants = plants.filter(plant => plant.name.toLowerCase().includes(searchTerm.toLowerCase()))
+
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm onAddPlant={handleAddPlant} />
+      <Search searchTerm={searchTerm} onSearchChange={handleSearchChange} />
+      <PlantList plants={filteredPlants} onToggleSoldOut={handleToggleSoldOut} />
     </main>
   );
 }
