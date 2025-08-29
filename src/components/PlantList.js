a401248c35415c1154efc209fe75c1a354a6437c
maranimitchelle@gmail.com <mitch@DESKTOP-PMCR53J.localdomain>
2025-08-29 07:31:53 +0300

    display plants

--- src/components/PlantList.js
+++ src/components/PlantList.js
@@ -4,6 +4,7 @@ import PlantCard from "./PlantCard";
 function PlantList({plants, onToggleSoldOut }) {
   return (
     <ul className="cards">
+
       {/* map over plants array to create each plant card */}
       {plants.map((plant) => (
       <PlantCard key={plant.id} plant={plant} onToggleSoldOut={onToggleSoldOut} />
